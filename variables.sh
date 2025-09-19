39b37f90607bb4f682f1ff8d5e695e6768d7b0e1
murali <murali.pydi@gmail.com>
2025-09-19 23:53:22 +0530

    commited for variables

--- variables.sh
+++ variables.sh
@@ -5,6 +5,6 @@ SUM=$((PERSON1+PERSON2))
 echo "SUM is :$SUM"
 LEADERS=("MURALI" "krishna" "pydi" "radha")
 echo "first leader is:${LEADERS[0]}" 
-echo "last reader is:${LEADERS[3]}"
+echo "last leader is:${LEADERS[3]}"
 echo "ALL LEADERS ARE:${LEADERS[@]}"    
 echo "other leaders are:${LEADERS[10]}"
