27f13cd71bf4ac1c9d06ecb9a34037fee4a1e929
murali <murali.pydi@gmail.com>
2025-09-22 23:58:37 +0530

    commited for expression

--- expression.sh
+++ expression.sh
@@ -1,8 +1,8 @@
 #!/bin/ bash
 echo "please enter the NUMBER1"
-read $NUMBER1
+read NUMBER1
 echo "please enter the NUMBER2"       
-read $NUMBER2
+read NUMBER2
 echo "SCRIPT NAME IS:$0"
 echo "number 1 is:$NUMBER1"
 echo "number 2 is:$NUMBER2"
