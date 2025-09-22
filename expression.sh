2050bcf25c5e2ce91eb780f64e8afa872c14429d
murali <murali.pydi@gmail.com>
2025-09-22 23:31:44 +0530

    commited for expression

---
+++ expression.sh
@@ -0,0 +1,11 @@
+#!/bin/ bash
+NUMBER=$1
+NUMBER2=$2
+echo "SCRIPT NAME IS:$0"
+echo "number 2 is:$NUMBER2"
+if [ (($NUMBER) % 2) -eq  0 ];then
+echo "Given number $NUMBER is even number"
+else 
+echo "Given number $NUMBER is not even number"
+fi
+echo "sum of number 1 and number 2 is :$(($1+$2))"
