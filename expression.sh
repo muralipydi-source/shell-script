06381fda1e1ef5d7d849e5265301a00eefed06b3
murali <murali.pydi@gmail.com>
2025-09-22 23:49:41 +0530

    commited for expression

--- expression.sh
+++ expression.sh
@@ -1,11 +1,13 @@
 #!/bin/ bash
-NUMBER=$1
-NUMBER2=$2
+echo "please enter the NUMBER1"
+read $NUMBER1
+echo "please enter the NUMBER2"       
+read $NUMBER2
 echo "SCRIPT NAME IS:$0"
 echo "number 2 is:$NUMBER2"
 if [ $(($NUMBER % 2)) -eq  0 ];then
 echo "Given number $NUMBER is even number"
 else 
-echo "Given number $NUMBER is not even number"
+echo "Given number $NUMBER is odd"
 fi
 echo "sum of number 1 and number 2 is :$(($1+$2))"
